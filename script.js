32d1a6e164190e67682575163a93b496f6b0139f
Nguyễn Bảo Long <longbao2904@gmail.com>
2026-01-04 09:20:05 +0700

    feat: implement win and tie game logic
    - Add winning combinations array and checkWin helper function.
    - Create checkResult to determine game state (win, tie, or continue).
    - Update playRound to announce winner or tie game in console.

--- script.js
+++ script.js
@@ -22,11 +22,51 @@ function createPlayer(name, marker) {
 const GameController = (function () {
   const player1 = createPlayer("Long", "X");
   const player2 = createPlayer("Sim", "O");
-
   let activePlayer = player1;
+
+  const board = Gameboard.getBoard();
+
+  const winCondition = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const checkWin = function () {
+    for (let condition of winCondition) {
+      const [a, b, c] = condition;
+
+      if (board[a] !== "" && board[a] === board[b] && board[a] === board[c]) {
+        return true;
+      }
+    }
+
+    return false;
+  };
+
+  const checkResult = function () {
+    if (checkWin()) {
+      return "win";
+    }
+    if (!board.includes("")) {
+      return "tie";
+    }
+    return "continue";
+  };
 
   const playRound = function (index) {
     if (Gameboard.placeMarker(index, activePlayer.marker)) {
+      if (checkResult() === "win") {
+        return `${activePlayer.name} IS THE WINNER!`;
+      } else if (checkResult() === "tie") {
+        return "TIE GAME!";
+      }
+
       activePlayer = activePlayer === player1 ? player2 : player1;
     }
 
