9ed3479462a48eea13a547cd38f7bdec57557bc2
Nguyễn Bảo Long <longbao2904@gmail.com>
2026-01-04 09:18:33 +0700

    feat: implement GameController with turn-switching
    - Create GameController module to manage game flow and players.
    - Integrate Gameboard.placeMarker with turn validation.
    - Ensure active player only switches upon a successful move.

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ const Gameboard = (function () {
   const placeMarker = function (index, marker) {
     if (board[index] === "") {
       board[index] = marker;
+      return true;
     }
 
-    return board;
+    return false;
   };
 
   return { getBoard, placeMarker };
@@ -17,3 +18,20 @@ const Gameboard = (function () {
 function createPlayer(name, marker) {
   return { name, marker };
 }
+
+const GameController = (function () {
+  const player1 = createPlayer("Long", "X");
+  const player2 = createPlayer("Sim", "O");
+
+  let activePlayer = player1;
+
+  const playRound = function (index) {
+    if (Gameboard.placeMarker(index, activePlayer.marker)) {
+      activePlayer = activePlayer === player1 ? player2 : player1;
+    }
+
+    return Gameboard.getBoard();
+  };
+
+  return { playRound };
+})();
